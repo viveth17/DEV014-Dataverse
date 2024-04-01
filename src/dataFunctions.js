6ec48426c7ab9d84ea54351bcc23e28989413771
Mireilys <mireilysastudillo1991@gmail.com>
2024-04-01 11:01:55 -0300

    Funcion Filtrar y estilos filtro y ordenado

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,7 +1,7 @@
 export const filterData = (data, filterBy, value) => {
-  console.log('data', data);
-  console.log('filterBy', filterBy);
-  console.log('value', value);
+  // console.log('data', data);
+  // console.log('filterBy', filterBy);
+  // console.log('value', value);
   const filteredData = data.filter(item => {
     const filterParts = filterBy.split('.');
     let obj = item;
@@ -10,9 +10,41 @@ export const filterData = (data, filterBy, value) => {
     }
     return obj === value;
   });
-  console.log('filteredData', filteredData);
+  // console.log('filteredData', filteredData);
   // Devuelve el arreglo filtrado
   return filteredData;
 };
 
+
+// export const sortBySharkSize = (data, sortBy, sortOrder) => {
+//   // console.log('data', data);
+//   // console.log('sortBy', sortBy);
+//   // console.log('sortOrder', sortOrder);
+//   const sortedData = data.sort((a, b) => {
+//     let sizeA, sizeB;
+
+//     // Obtener los tamaños de los tiburones basado en el campo especificado por sortBy
+//     if (sortBy) {
+//       sizeA = a[sortBy];
+//       sizeB = b[sortBy];
+//     } else {
+//       sizeA = a.maximumSizeMtr;
+//       sizeB = b.maximumSizeMtr;
+//     }
+
+//     // Determinar el orden de comparación basado en sortOrder
+//     const compareValue = sortOrder === 'desc' ? -1 : 1;
+
+//     // Realizar la comparación de tamaños
+//     if (sizeA < sizeB) {
+//       return -1 * compareValue;
+//     }
+//     if (sizeA > sizeB) {
+//       return 1 * compareValue;
+//     }
+//     return 0;
+//   });
+
+//   return sortedData;
+// };
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { filterData } from './dataFunctions.js';
+// import { sortBySharkSize } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/dataset.js';
 
 
@@ -8,13 +8,13 @@ const htmlElement = document.querySelector('html');
 
 htmlElement.getElementsByTagName('body')[0].getElementsByTagName('div')[0].appendChild(renderItems(data));
 
-// Para seleccionar el <select>
+// Para seleccionar el <select> Filtrar
 const selectElement = document.getElementsByName("locationOfTheSpecie")[0];
 // Evento al elemento <select> 
 selectElement.addEventListener('change', function () {
 // capturar el valor seleccionado 
   const selectedValue = selectElement.value;
-  console.log('Opción seleccionada:', selectedValue);
+  // console.log('Opción seleccionada:', selectedValue);
 
   //filtrar los datos basados en el valor seleccionado
   const filteredData = filterData(data, 'facts.locationOfTheSpecie', selectedValue);
@@ -23,8 +23,29 @@ selectElement.addEventListener('change', function () {
   htmlElement.getElementsByTagName('body')[0].getElementsByTagName('div')[0].replaceChildren(renderItems(filteredData));
 });
 
+// // Para seleccionar el <select> Ordenar
+// const sortElement = document.getElementsByName("maximumSizeMtr")[0];
 
+// // Evento al elemento <select> 
+// sortElement.addEventListener("change", function () {
+//   // Capturar el valor seleccionado 
+//   const sortValue = sortElement.value;
 
-// console.log(example, renderItems(data), data);
+//   // Ordenar la data basada en el valor seleccionado
+//   let sortedData;
+//   if (sortValue === 'asc') {
+//     sortedData = sortBySharkSize(data, 'maximumSizeMtr', 'asc');
+//   } else if (sortValue === 'desc') {
+//     sortedData = sortBySharkSize(data, 'maximumSizeMtr', 'desc');
+//   } else {
+//     // Manejar un caso por defecto
+//     sortedData = data;
+//   }
+
+//   // Renderizar los elementos ordenados
+//   htmlElement.getElementsByTagName('body')[0].getElementsByTagName('div')[0].replaceChildren(renderItems(sortedData));
+// });
+
+// // console.log(example, renderItems(data), data);
 
 renderItems(data);
